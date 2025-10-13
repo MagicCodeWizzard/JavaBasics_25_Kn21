0a02cd7810d589b5b0e1ad850c62f83ead571230
Andrii Kuliak <andrii.kuliak@gmail.com>
2025-10-13 08:26:42 +0300

    Initial commit

---
+++ src/main/java/org/college/algoritmization/practice1/Main.java
@@ -0,0 +1,17 @@
+package org.college.algoritmization.practice1;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int x;
+
+        Scanner s = new Scanner(System.in);
+        x = s.nextInt();
+        s.nextLine();
+
+        System.out.println("X = " + x);
+
+        s.close();
+    }
+}
