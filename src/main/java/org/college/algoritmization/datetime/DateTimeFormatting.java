67fa6d1e639dc0a1207f1058b04abbe29932c7e0
Andrii Kuliak <andrii.kuliak@gmail.com>
2026-04-13 08:03:54 +0300

    Date time formatting example added

---
+++ src/main/java/org/college/algoritmization/datetime/DateTimeFormatting.java
@@ -0,0 +1,9 @@
+package org.college.algoritmization.datetime;
+
+import java.time.format.DateTimeFormatter;
+
+public class DateTimeFormatting {
+    public static void main(String[] args) {
+
+    }
+}
